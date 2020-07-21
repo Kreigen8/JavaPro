98d02f8e641e26639d70a2dcda86b73dd28894f9
Kreigen <kreigen8@gmail.com>
2020-07-21 12:08:41 +0300

    первое и второе задание

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -8,7 +8,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-   
+        Integer[] arr = {1, 3, 5, 7, 9, 0, 8, 6, 4, 2};
+        change(arr,0, 9);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println();
+
+        ArrayList<Integer> arrList = arrToList(arr);
+        System.out.println(arrList);
+
+    }
+
+    public static <T> ArrayList<T> arrToList(T[] arr){
+        return new ArrayList<>(Arrays.asList(arr));
+    }
+
+    public static void change(Object[] arr, int i, int j) {
+        Object firstElement = (int) arr[i];
+        arr[i] = arr[j];
+        arr[j] = firstElement;
+    }
 
 
 }
